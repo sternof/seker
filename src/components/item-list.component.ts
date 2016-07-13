fd89a501f0977e1f521fbd2d970402997c12a46d
nir@500tech.com <nir@500tech.com>
2016-07-13 21:08:10 +0300

    template

--- src/components/item-list.component.ts
+++ src/components/item-list.component.ts
@@ -1,16 +1,14 @@
 import {Component} from '@angular/core';
+import {TodolistItemComponent} from "./todolist-item.component";
 
 @Component({
   selector:'item-list',
+  directives:[TodolistItemComponent],
   template:`
-<ul class="todo-list">
-
-      
-
-      
-    </ul>
-    <!-- /List-->
-`
+    <ul class="todo-list">      
+      <todolist-item></todolist-item>
+    </ul>    
+   `
 })
 
-export class ItemList {}
+export class ItemListComponent {}
--- src/components/todolist.component.ts
+++ src/components/todolist.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {TodolistHeaderComponent} from "./todolist-header.component";
+import {ItemListComponent} from "./item-list.component";
+import {TodolistFooterComponent} from "./todolist-footer.component";
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'todolist',
-  styles:['div {text-align: center}'],
+  styleUrls: ['./todolist.css'],
+  directives:[
+      TodolistHeaderComponent,
+      ItemListComponent,
+      TodolistFooterComponent
+  ],
   template: `
     <section class="todoapp">
+      
+      <todolist-header></todolist-header>
+            
       <section class="main">
 
-    <input class="toggle-all"
+      <input class="toggle-all"
            type="checkbox">
     
       </section>
+      
+      <item-list></item-list>
+      
+      <todolist-footer></todolist-footer>
+      
 </section>
 
     `,

fc4120752b01b81db48e114eea09dc3008c12cbc
nir@500tech.com <nir@500tech.com>
2016-08-01 11:11:45 +0300

    logic

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,8 +1,10 @@
-import {Component} from "@angular/core";
+import {Component, ViewEncapsulation} from "@angular/core";
 import {TodolistComponent} from "./todolist.component";
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'app',
+  styleUrls: ['todolist.css'],
   directives:[TodolistComponent],
   template: `<todolist></todolist>`,
 })
--- src/components/todolist.component.ts
+++ src/components/todolist.component.ts
@@ -19,4 +19,36 @@ import {TodolistFooterComponent} from "./todolist-footer.component";
    `,
 })
 
-export class TodolistComponent {}
+export class TodolistComponent {
+
+  private title: string;
+  private items: Item[];
+
+  constructor() {
+    this.title = 'TODOS';
+    this.items = [];
+  }
+
+  addItem(text: string){
+    this.items.push(new Item(text))
+  }
+
+  removeItem(item:Item){
+    let index = this.items.indexOf(item);
+    this.items.splice(index, 1);
+  }
+
+}
+
+class Item {
+  public text:string;
+  public done:boolean;
+  public editMode:boolean;
+
+  constructor(text: string) {
+    this.text     = text;
+    this.done     = false;
+    this.editMode = false;
+  }
+
+}

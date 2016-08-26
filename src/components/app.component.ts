71b738103d83ee9320138b1d1af0f6f818c753de
Shmuela <shmuela@500tech.com>
2016-08-27 00:09:44 +0300

    break down to components

--- src/components/app.component.ts
+++ src/components/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'aah-app',
   styleUrls: ['./app.component.css'],
-  templateUrl: './app.component.html',
-  directives: [HeaderComponent]
+  templateUrl: './app.component.html'
+
 })
 
 export class AppComponent {}
--- src/components/header/header.component.ts
+++ src/components/header/header.component.ts
@@ -2,66 +2,18 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'aah-header',
-
-  styles: [
-    `
-      .header h1 {
-        position: absolute;
-        top: -155px;
-        width: 100%;
-        font-size: 100px;
-        font-weight: 100;
-        text-align: center;
-        color: rgba(175, 47, 47, 0.15);
-        -webkit-text-rendering: optimizeLegibility;
-        -moz-text-rendering: optimizeLegibility;
-        text-rendering: optimizeLegibility;
-      }
-      
-      .new-todo {
-        position: relative;
-        margin: 0;
-        width: 100%;
-        font-size: 24px;
-        font-family: inherit;
-        font-weight: inherit;
-        line-height: 1.4em;
-        border: 0;
-        outline: none;
-        color: inherit;
-        box-sizing: border-box;
-        -webkit-font-smoothing: antialiased;
-        -moz-font-smoothing: antialiased;
-        font-smoothing: antialiased;
-        padding: 16px 16px 16px 60px;
-        border: none;
-        background: rgba(0, 0, 0, 0.003);
-        box-shadow: inset 0 -2px 1px rgba(0,0,0,0.03);
-      }
-    `
-  ],
+  styleUrls: ['./header.component.css'],
 
   template: `
-    <header class="header">
-  
-      <!-- Header Title-->
-      <h1>{{ title }}</h1>
-  
-      <!--Header Form-->
-      <input class="new-todo"
-             placeholder="What needs to be done?"
-             autofocus #todoInput 
-             (keyup.enter)="changeTitle(todoInput.value)" >
+    <header class="header">  
+      <h1>{{ title }}</h1>  
+      <aah-todo-input></aah-todo-input>
     </header>
   `
 })
 
 export class HeaderComponent {
 
-  title:string = 'AfterHours Todo';
-
-  changeTitle(newTitle: string) {
-    this.title = newTitle;
-  }
+  title: string = 'Todos';
 
 }

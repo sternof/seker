f8f526747579c7931944d620fdf8962b317965d4
Jazzmin <jazz@Jazzmins-MacBook.local>
2016-10-26 17:58:41 +0300

    moved components to array

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
 import { RouterModule }   from '@angular/router';
-
+import {LocationStrategy, HashLocationStrategy} from "@angular/common";
 
 import { AppComponent } from './components/app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { TodoItemComponent } from './components/todo-item/todo-item.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { ResultsComponent } from './components/results/results.component';
 import { QuestionsPageComponent } from './components/questions-page/questions-page.component';
 import { HomeComponent} from './components/home/home.component';
@@ -19,18 +15,18 @@ import { AnalysisService} from './services/analysis.service';
 import { ListService} from './services/list.service';
 
 
-import {LocationStrategy, HashLocationStrategy} from "@angular/common";
+
 import {Store} from "./app.store";
 import {APP_ACTIONS} from "./actions/app.actions";
-//import {APP_COMPONENTS} from "./components/app.components";
+import {APP_COMPONENTS} from "./components/app.components";
 
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpModule, JsonpModule,
       RouterModule.forRoot([
-  //    { path: 'links', component: linksComponent },
+  //    { path: 'links', component: LinksPageComponent },
       { path: 'results', component: ResultsComponent },
       { path: 'questions', component: QuestionsPageComponent },
-    //  { path: 'links', component: ResultsComponent },
+    //  { path: 'addquestions', component: AddQuestionsComponent },
 /*      
       { path: 'hero/:id', component: HeroDetailComponent },
 {
@@ -42,18 +38,8 @@ import {APP_ACTIONS} from "./actions/app.actions";
       },*/
       { path: '', component: HomeComponent },
       { path: '**', component: HomeComponent }
-    //  { path: '**', component: PageNotFoundComponent }
     ])],
-  declarations: [
-    AppComponent, 
-    HeaderComponent, 
-    FooterComponent, 
-    TodoListComponent, 
-    TodoItemComponent,
-    QuestionsPageComponent,
-    ResultsComponent,
-    HomeComponent,
-    LinksComponent
+  declarations: [...APP_COMPONENTS
   ],
   providers: [ 
     {
---
+++ src/components/app.components.ts
@@ -0,0 +1,21 @@
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
+import { TodoItemComponent } from './todo-item/todo-item.component';
+import { FooterComponent } from './footer/footer.component';
+import { ResultsComponent } from './results/results.component';
+import { QuestionsPageComponent } from './questions-page/questions-page.component';
+import { HomeComponent} from './home/home.component';
+import { LinksComponent} from './links/links.component';
+
+export const APP_COMPONENTS = 
+
+[   AppComponent, 
+    HeaderComponent, 
+    FooterComponent, 
+    TodoListComponent, 
+    TodoItemComponent,
+    QuestionsPageComponent,
+    ResultsComponent,
+    HomeComponent,
+    LinksComponent ]

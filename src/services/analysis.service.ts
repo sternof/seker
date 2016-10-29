9e36a8d5ef6fe8990f0c7d931a1bf86c9976041c
Jazzmin <jazz@Jazzmins-MacBook.local>
2016-10-29 20:36:48 +0300

    upd analysis calcs

--- src/services/analysis.service.ts
+++ src/services/analysis.service.ts
@@ -20,18 +20,29 @@ calculateResult(list) : number {
 let percent = this.calculatePercent(list);
 let res : number = 0;
 if (percent !== 0) { 
-    res = this.calculateFromPercentToYearAll( percent);
+    res = this.calculateFromPercentToYearHLJewishPop( percent);
  }
 return res;
 }
 
-private calculateFromPercentToYearAll(percent: number) : number {
+private calculateFromPercentToYearAllGenPop(percent: number) : number {
 let res = Math.round((percent+13.642)/0.006956);
 return res; 
 }
-
-private calculateFromPercentToYearHL(percent: number) : number {
+// HL - Haredim plus Leumiim
+private calculateFromPercentToYearHLGenPop(percent: number) : number {
 let res = Math.round((percent+11.689)/0.0065908);
+return res; 
+}
+
+private calculateFromPercentToYearAllJewishPop(percent: number) : number {
+let res = Math.round((percent+17.681)/0.009014);
+return res; 
+}
+
+// HL - Haredim plus Leumiim
+private calculateFromPercentToYearHLJewishPop(percent: number) : number {
+let res = Math.round((percent+14.89)/0.007517);
 return res; 
 }
 
